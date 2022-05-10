153396ce35693f51c85bc67a8b56d3471ad7699f
Vedad Coric <c18vcc@cs.umu.se>
2022-05-10 14:34:03 +0200

    removing garbage files in main

--- stupid.py
+++
@@ -1,9 +0,0 @@
-from ctypes import sizeof
-
-
-a = ["G", "H", "I", "J", "K"]
-for j in range (len(a)):
-    for i in range (6, 17):
-        print('=('+a[j]+'4/'+a[j] + str(i) + ') ', end="")
-        #print('=({}4/{}{}) ' + a[j] + a[j] + str(i), end="")
-    print("\n")
